2342ea59c1028a621bb96a63f62a8667763f3986
Lewis Nelson <lewis.nelson@iohk.io>
2020-04-09 09:39:26 +0100

    Contain Tab navigation

--- src/components/Header/DesktopNavigation.js
+++ src/components/Header/DesktopNavigation.js
@@ -4,30 +4,33 @@ import PropTypes from 'prop-types'
 import { Location } from '@reach/router'
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
+import Box from '@material-ui/core/Box'
 import Link from '../Link'
 
 const DesktopNavigation = ({ path }) => (
   <Query
     render={items => (
-      <Tabs
-        value={path}
-        indicatorColor='primary'
-        textColor='primary'
-        variant='scrollable'
-        scrollButtons='on'
-        aria-label='scrollable auto tabs example'
-      >
-        {items.map(({ label, path }) => (
-          <Tab
-            label={label}
-            aria-controls={`scrollable-auto-tabpanel-${label.toLowerCase().replace(/ /g, '-')}`}
-            key={path}
-            href={path}
-            component={forwardRef((props, ref) => <Link {...props} {...ref} />)}
-            value={path}
-          />
-        ))}
-      </Tabs>
+      <Box maxWidth='100vw'>
+        <Tabs
+          value={path}
+          indicatorColor='primary'
+          textColor='primary'
+          variant='scrollable'
+          scrollButtons='on'
+          aria-label='scrollable auto tabs example'
+        >
+          {items.map(({ label, path }) => (
+            <Tab
+              label={label}
+              aria-controls={`scrollable-auto-tabpanel-${label.toLowerCase().replace(/ /g, '-')}`}
+              key={path}
+              href={path}
+              component={forwardRef((props, ref) => <Link {...props} {...ref} />)}
+              value={path}
+            />
+          ))}
+        </Tabs>
+      </Box>
     )}
   />
 )
